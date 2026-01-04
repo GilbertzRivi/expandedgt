7f63d6ec2197ca8faf5129fc85dafcd8158cf0e7
Kolja <contact@kolja.lu>
2026-01-04 07:29:32 +0100

    expanded stocking input

--- src/main/kotlin/lu/kolja/expandedgt/xmod/ExpAEFluidConfigWidget.kt
+++ src/main/kotlin/lu/kolja/expandedgt/xmod/ExpAEFluidConfigWidget.kt
@@ -7,12 +7,12 @@ import com.gregtechceu.gtceu.integration.ae2.slot.ExportOnlyAEFluidList
 import com.gregtechceu.gtceu.integration.ae2.slot.ExportOnlyAEFluidSlot
 import com.gregtechceu.gtceu.integration.ae2.slot.IConfigurableSlot
 
-class ExpAEFluidConfigWidget(x: Int, y: Int, val list: ExportOnlyAEFluidList): ConfigWidget(x, y, list.inventory, list.isStocking) {
+class ExpAEFluidConfigWidget(x: Int, y: Int, val list: ExportOnlyAEFluidList, slots: Array<ExportOnlyAEFluidSlot>): ConfigWidget(x, y, slots, list.isStocking) {
     override fun init() {
         this.displayList = Array<IConfigurableSlot>(config.size) { ExportOnlyAEFluidSlot() }
         this.cached = Array<IConfigurableSlot>(config.size) {
-            val line = it / 16
-            this.addWidget(AEFluidConfigSlotWidget((it - line * 16) * 18, line * 38, this, it))
+            val line = it / 8
+            this.addWidget(AEFluidConfigSlotWidget((it - line * 8) * 18, line * 38, this, it))
             return@Array ExportOnlyAEFluidSlot()
         }
     }
--- src/main/kotlin/lu/kolja/expandedgt/xmod/ExpAEItemConfigWidget.kt
+++ src/main/kotlin/lu/kolja/expandedgt/xmod/ExpAEItemConfigWidget.kt
@@ -7,12 +7,12 @@ import com.gregtechceu.gtceu.integration.ae2.slot.ExportOnlyAEItemList
 import com.gregtechceu.gtceu.integration.ae2.slot.ExportOnlyAEItemSlot
 import com.gregtechceu.gtceu.integration.ae2.slot.IConfigurableSlot
 
-class ExpAEItemConfigWidget(x: Int, y: Int, val list: ExportOnlyAEItemList): ConfigWidget(x, y, list.inventory, list.isStocking) {
+class ExpAEItemConfigWidget(x: Int, y: Int, val list: ExportOnlyAEItemList, slots: Array<ExportOnlyAEItemSlot>): ConfigWidget(x, y, slots, list.isStocking) {
     override fun init() {
         this.displayList = Array<IConfigurableSlot>(config.size) { ExportOnlyAEItemSlot() }
         this.cached = Array<IConfigurableSlot>(config.size) {
-            val line = it / 16
-            this.addWidget(AEItemConfigSlotWidget((it - line * 16) * 18, line * 38, this, it))
+            val line = it / 8
+            this.addWidget(AEItemConfigSlotWidget((it - line * 8) * 18, line * 38, this, it))
             return@Array ExportOnlyAEItemSlot()
         }
     }
